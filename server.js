f4f4ce91ffe998dad39be89748ab309ffc1dc31e
Chris-L985 <thelopezdispenser@gmail.com>
2021-07-07 23:41:35 -0600

    added server to application

---
+++ server.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const mysql = require('mysql2');
+const inputCheck = require('./utils/inputCheck');
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+//connect to database
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        user: 'root',
+        password: '',
+        database: 'election'
+    },
+    console.log('Connected to the election database.')
+);
+
+app.get('/', (req, res) => {
+    res.json({
+        message: 'Hello World'
+    });
+});
+
+app.use((req, res) => {
+    res.status(404).end();
+});
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
